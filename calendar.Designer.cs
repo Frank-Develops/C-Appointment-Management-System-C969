63c388c9c80ccdcc7dc8f7c3931dfc2c3a393641
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-07-02 15:11:34 -0500

    fixed scheduling

---
+++ calendar.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace C969_FB
+{
+    partial class calendar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // goBack
+            // 
+            this.goBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.goBack.Location = new System.Drawing.Point(738, 428);
+            this.goBack.Name = "goBack";
+            this.goBack.Size = new System.Drawing.Size(191, 90);
+            this.goBack.TabIndex = 0;
+            this.goBack.Text = "Go Back to Appointment Manager";
+            this.goBack.UseVisualStyleBackColor = true;
+            this.goBack.Click += new System.EventHandler(this.goBack_Click);
+            // 
+            // calendar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(989, 553);
+            this.Controls.Add(this.goBack);
+            this.Name = "calendar";
+            this.Text = "calendar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goBack;
+    }
+}
---
+++ calendar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C969_FB
+{
+    public partial class calendar : Form
+    {
+        public calendar()
+        {
+            InitializeComponent();
+        }
+
+        private void goBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Appointments appointments = new Appointments();
+            appointments.Show();
+        }
+    }
+}
