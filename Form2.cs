5f270bd3149821ff70d9323f9fba60392180b5c1
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-07-02 23:14:01 -0500

    logon trace works

--- Form2.cs
+++ Form2.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Management;
 using System.Text;
@@ -93,7 +94,16 @@ namespace C969_FB
                         };
                     }
 
-                   
+                    string writeUser = "test";
+                    string writeTime = currentTime.ToString();
+                    string documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(documentPath, "Login_History.txt"), true))
+                    {
+                        outputFile.WriteLine("Username: " + writeUser + " logon time: " + writeTime );
+                       
+                    }
+
 
                     reader = sqlCommand.ExecuteReader();
                     reader.Close();
